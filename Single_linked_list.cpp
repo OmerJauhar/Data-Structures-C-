9314d3a74c7af75174275fb578d5879c4cc48fd3
Omer Jauharm <umerjouhar31@gmail.com>
2022-09-14 22:58:31 +0500

    meow

--- Single_linked_list.cpp
+++ Single_linked_list.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
+#include<iomanip>
 using namespace std ;
 
 class node {
 ;
 public:
-
     node* next ;
     int info;
 public:
     node()
     {
-        this->next = NULL ; 
+        this->next = NULL ;
     }
     node(int val )
     {
@@ -24,8 +24,8 @@ class sll
 {
 
     node* head;
-    node* temp ; 
-    node* temp1 ; 
+    node* temp ;
+    node* temp1 ;
 public:
     sll()
     {
@@ -33,42 +33,77 @@ public:
     }
     void insert_node() ;
     void print_list() ;
+    void set_head() ;
     void delete_list() ;
+
 
 };
 void sll::insert_node() /* position and data of the new node   */
 {
     int key ;
-    temp = new node () ; 
-    head = new node () ; 
-    temp = head ; 
-    if (head = NULL)
+    temp = new node () ;
+    temp = head ;
+    if (head == nullptr)
     {
         cout<<"Enter the value you want  to store in head node "<<endl ;
+        cin>>head->info;
         return;
     }
-    cout<<"Enter key:"<<endl; 
+    cout<<"Enter key:"<<endl;
     cin>>key  ;
-    
-    while(temp != NULL)
+
+    while(temp != nullptr)
     {
         if(temp->info == key)
         {
             temp1 = new node ;
-            cout<<"Enter the info for the new node "<<endl ; 
-            cin>>temp1->info ; 
+            cout<<"Enter the info for the new node "<<endl ;
+            cin>>temp1->info ;
             temp1->next = temp->next ;
             temp->next = temp1 ;
         }
-        temp = temp->next ; 
+        temp = temp->next ;
     }
 
 
+}
+void sll::print_list()
+{
+    temp = head ;
+    if(head == nullptr )
+    {
+        cout<<"Shit! The list is empty"<<endl ;
+        return ;
+    }
+    while(temp!= nullptr)
+    {
+        cout<<setw(4)<<temp->info<<" " ;
+        temp = temp->next ;
+    }
+}
+
+void sll::set_head()
+{
+    if(head == nullptr)
+    {
+        cout<<"Enter the value you want to store in the head "<<endl ;
+        cin>>head->info ;
+//        return ;
+    }
+    cout<<"The head already has a value of "<<head->info<<endl ;
 }
 
 int main()
 {
     cout<<"Start of the program "<<endl ;
+    sll s1 ;
+    s1.set_head();
+    s1.insert_node();
+    s1.insert_node();
+    s1.insert_node();
+    s1.insert_node();
+    s1.print_list() ; 
+
 
     return 0;
 }
